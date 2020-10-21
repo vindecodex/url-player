4ea2fec941e7cfa6a28a3d8c5d828f2edc2361ee
Vincent Villaluna <villalunavincent@gmail.com>
2020-10-21 16:48:15 +0800

    :tada: :tada: initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app,  BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 500,
+    height: 300,
+    webPreferences: {
+      nodeIntegration:  true
+    }
+  })
+
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
